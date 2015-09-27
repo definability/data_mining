7f239301c94fe1c2f8f0339affbaa9aefe13e29e
char-lie <5962274+char-lie@users.noreply.github.com>
2015-09-27 18:11:06 +0300

    Printing numbers in CSV format

--- lab1/draw_plot.py
+++ lab1/draw_plot.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python
 
 from sys import stdin
+from os import linesep
 from matplotlib import pyplot
 
+import numpy
+from scipy import optimize
+
 frequencies = [int(n) for l in stdin for n in l.strip().split(' ')]
+
+print linesep.join('%d'%f for f in frequencies)
 
 pyplot.plot(frequencies, 'ro')
 
