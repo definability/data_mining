638f69ff34bcf74b071abe216f0d0f2a35685c23
char-lie <5962274+char-lie@users.noreply.github.com>
2015-10-02 11:20:33 +0300

    Made lab2

---
+++ lab2/count.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from sys import stdin
+
+words = ' '.join([l.strip() for l in stdin]).split(' ')
+
+found = []
+
+for i, w in enumerate(words):
+    if w not in found:
+        found.append(w)
+    print '%d,%d'%(i, len(found))
+
