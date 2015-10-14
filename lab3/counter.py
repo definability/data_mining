e37b9411f41381d3eca27c7c13870f554a75622c
char-lie <5962274+char-lie@users.noreply.github.com>
2015-10-14 18:03:55 +0300

    Removed redundant comments

--- lab3/counter.py
+++ lab3/counter.py
@@ -5,6 +5,7 @@ from sys import stdin
 from os import linesep
 from math import log
 
+
 def get_count(words):
     tfs = {}
     for key in set(words):
@@ -13,19 +14,24 @@ def get_count(words):
         tfs[w] += 1
     return tfs
 
+
 texts = ([l.strip().split(' ') for l in stdin])
 names = map(lambda text: text[0], texts)
 texts = map(lambda text: text[1:], texts)
 
+
 tfs = map(get_count, texts)
+
 
 idf = {}
 for word in set(sum(texts, [])):
     idf[word] = 0
 
+
 for tf in tfs:
     for word in tf:
         idf[word] += 1
+
 
 logN = log(len(texts))
 for word in idf:
@@ -38,8 +44,6 @@ for i, tf in enumerate(tfs):
     for word in tf:
         tf_idfs[i][word] = float(tf[word] * idf[word]) / len(tf)
 
-#result = sorted(counts.iteritems(),key=lambda x: x[1],
-#                reverse=True)
 
 print linesep.join('%s,%s,%f'%(names[i], word, tf_idf[word])
                     for i, tf_idf in enumerate(tf_idfs) for word in tf_idf)
