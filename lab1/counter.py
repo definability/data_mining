f88bded9210e8da0e31d492734a66b0d56c734db
char-lie <5962274+char-lie@users.noreply.github.com>
2015-10-16 00:08:21 +0300

    Fixed lab1: not only onique frequencies are needed

--- lab1/counter.py
+++ lab1/counter.py
@@ -2,15 +2,18 @@
 # -*- coding: utf-8 -*-
 
 from sys import stdin
+from os import linesep
 
-words = sorted(' '.join([l.strip() for l in stdin]).split(' '))
+words = ' '.join([l.strip() for l in stdin]).split(' ')
 
 counts = {}
-
 for key in set(words):
     counts[key] = 0
 for w in words:
     counts[w] += 1
 
-print ' '.join(map(str, sorted(list(set(counts.values())), reverse=True)))
+result = sorted(counts.iteritems(),key=lambda x: x[1],
+                reverse=True)
+
+print linesep.join('%s,%d'%r for r in result)
 
