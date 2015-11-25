f0e62956e7426b4e71df3526b6ff609caaef8591
char-lie <5962274+char-lie@users.noreply.github.com>
2015-11-25 23:55:43 +0200

    Added comments to lab4

--- lab4/main.py
+++ lab4/main.py
@@ -5,12 +5,13 @@ from math import log10
 
 
 def get_vertex(name, vertices):
-    # If we already have this vertex, just use it
+    # If we already have this vertex, just use it from cache
     if name in vertices:
         return vertices[name]
     # Otherwise we have to create new one
     a = g.add_vertex()
     word[a] = name
+    # Add the vertex to cache
     vertices[name] = a
     return a
 
@@ -38,8 +39,12 @@ if __name__ == '__main__':
         b = get_vertex(w[1], vertices)
         e = g.add_edge(a, b)
         # Set weight for new edge (tf-idf)
+        # Just empirical formula, which gives edges with 1 <= width < 30
         weight[e] = log10(w[2])*10 + 23
 
+    # Draw the graph;
+    # Weight is responsible for edges widths
+    # Word contains labels for vertices
     graph_draw(g, vertex_font_size=12, edge_pen_width=weight, vertex_text=word,
                output='words.png', output_size=(2000, 2000))
 
