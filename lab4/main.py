6a3d1c06c582889ec2068edfd0733bbc6c311b2e
char-lie <5962274+char-lie@users.noreply.github.com>
2015-11-25 23:47:03 +0200

    Added files for lab4

---
+++ lab4/main.py
@@ -0,0 +1,45 @@
+from graph_tool.all import *
+from words import words
+from valuable import words as dictionary
+from math import log10
+
+
+def get_vertex(name, vertices):
+    # If we already have this vertex, just use it
+    if name in vertices:
+        return vertices[name]
+    # Otherwise we have to create new one
+    a = g.add_vertex()
+    word[a] = name
+    vertices[name] = a
+    return a
+
+
+if __name__ == '__main__':
+
+    # Create directed graph
+    g = Graph(directed=True)
+
+    # Create 'weight' property for edge: will contain tf-idf
+    weight = g.new_edge_property('float')
+    # Create 'word' property for vertex: will contain string with current word
+    word = g.new_vertex_property('string')
+    # Dictionary with existent vertices (cache)
+    vertices = {}
+
+    for w in words:
+        # Check, whether at least one word is in dictionary of valuable words
+        if w[0] not in dictionary and w[1] not in dictionary:
+            continue
+        # Check, whether at least one word is valuable
+        if dictionary[w[0]] < 0.1 and dictionary[w[1]] < 0.1:
+            continue
+        a = get_vertex(w[0], vertices)
+        b = get_vertex(w[1], vertices)
+        e = g.add_edge(a, b)
+        # Set weight for new edge (tf-idf)
+        weight[e] = log10(w[2])*10 + 23
+
+    graph_draw(g, vertex_font_size=12, edge_pen_width=weight, vertex_text=word,
+               output='words.png', output_size=(2000, 2000))
+
